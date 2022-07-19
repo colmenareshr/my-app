95f8e221f56246ca219ea2be1f752e43a321f3e6
Humberto Colmenares <colmenareshr@gmail.com>
2022-07-18 21:53:20 -0300

    Commit Inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
 
+import List from "./components/List/List";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Primeira aula ao vivo do React</h1>
+        <h2>Lista de compra</h2>
+        <List />
       </header>
+
     </div>
   );
 }
---
+++ src/components/List/List.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './List.css';
+
+function List() {
+   
+   return(
+      <ul>
+         <li>Manzana</li>
+         <li>Melón</li>
+         <li>Fresa</li>
+      </ul>
+   )
+}
+
+export default List;
